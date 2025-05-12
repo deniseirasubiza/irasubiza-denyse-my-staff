dd0ffc8fc21652a4adace34e71555388c7419af4
deniseirasubiza <160465975+deniseirasubiza@users.noreply.github.com>
2025-05-12 09:51:31 +0200

    Add files via upload

---
+++ 21-03-2024/ASSGNMT/html/Special Tags/variable.php
@@ -0,0 +1,24 @@
+<?php
+$abc=12;
+$total=0;
+&largest_soFar=0;
+$firstname="john";
+$second="Nzabagerageza";
+$second=" ";
+$names =$firstname.$space.$second;
+print("<center> <h1><u><i>just variables</i><u>")
+echo"<center><h1><i>$names</i></h1></center>";
+echo "the integer value is $abc\n";
+echo "$total++<br>";
+echo $largest_so_far--;
+echo "<center> <h1><u><i>variable name weirdness"
+
+//variable name weirdness
+$x = 2;
+$y = x+5;
+//print $y;
+print"<hr>"
+$z=5;
+$h=array("x"=>"hello;")
+print $h[$z];
+?>
